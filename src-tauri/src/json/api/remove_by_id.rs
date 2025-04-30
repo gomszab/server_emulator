3847f0b032c450418d5e2efb3006f537d18cdf5b
gomszab <gombosszabolcs88@gmail.com>
2025-04-30 22:35:51 +0200

    version upgrade, hotfix remove by id

--- src-tauri/src/json/api/remove_by_id.rs
+++ src-tauri/src/json/api/remove_by_id.rs
@@ -12,7 +12,7 @@ pub fn remove_by_id_json(
 ) -> Response<std::io::Cursor<Vec<u8>>> {
     if let Some(id) = path_param.get("id") {
         match facade.remove_by_id(id.clone()) {
-            Ok(_) => create_json_response(r#"{"error": "Item removed"}"#, 200),
+            Ok(_) => create_json_response(r#"{"message": "Item removed"}"#, 200),
             Err(_) => create_json_response(r#"{"error": "Item not found"}"#, 404),
         }
     } else {
